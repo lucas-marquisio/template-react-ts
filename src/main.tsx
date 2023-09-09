e2a0c45eef467634d6510e2cd340586549ae1b89
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-09-09 17:06:28 -0300

    feat: add provider store in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './services/routes'
+import { Provider } from 'react-redux'
+import { store } from './services/store/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
     <RouterProvider router={routes}/>
+  </Provider>
 )
