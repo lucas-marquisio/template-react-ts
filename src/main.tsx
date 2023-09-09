5e59bbdef3781659bb0fc93cb81610ef6ddc19b4
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-09-09 16:57:35 -0300

    fix: add routeBrowser main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './services/routes'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <RouterProvider router={routes}/>
 )
