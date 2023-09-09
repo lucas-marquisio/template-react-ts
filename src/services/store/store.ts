d4e51ccad17b78d2a9b8e589712212a7bb78bc67
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-09-09 17:04:13 -0300

    fix: import example store redux

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import Example from '@/services/store/example'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    Example
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
